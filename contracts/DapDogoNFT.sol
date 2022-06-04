088b57ab63852b0c7b7bb692e7fbffe49aecc69c
AmbujL <ambujlasyal@gmail.com>
2022-06-04 11:38:57 +0530

    Resolved -> some files were not uploaded in previous commit

---
+++ contracts/DapDogoNFT.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+
+contract DapDogoNFT is ERC721 ,Ownable 
+{
+  uint256 public mintPrice;
+  uint256 public totalSupply;
+  uint256 public maxSupply;
+  uint256 public maxPerWallet;
+  bool public isPublicMintEnabled;
+  string internal baseURI;
+  mapping(address=> uint256) public walletMint;
+
+
+  constructor() payable ERC721("DapDogo","DADO"){
+    mintPrice=0.1 ether;
+    totalSupply=0;
+    maxSupply=1000;
+    maxPerWallet=5;
+    // set wallet withdrawal address
+  }
+
+  function setPublicMintEnabled(bool _isPublicMintEnabled) external onlyOwner{
+
+    isPublicMintEnabled=_isPublicMintEnabled;
+  }
+
+
+  function setBaseTokenUri(string calldata _baseURI) external onlyOwner{
+    baseURI = _baseURI;
+  }
+
+  function tokenURI(uint256 _tokenId) public view override returns (string memory){
+    require(_exists(_tokenId),"Token does not exist !");
+    return string(abi.encodePacked(baseURI, Strings.toString(_tokenId),".json"));
+  }
+
+  function withdraw() external onlyOwner{
+    (bool success,)= owner().call{value:address(this).balance}("");
+    require(success,"withdraw failed");
+  }
+
+
+  function mint(uint256 _quantity) public payable {
+    require(isPublicMintEnabled,"minting not enabled");
+    require(msg.value == _quantity* mintPrice,"wrong mint value");
+    require(totalSupply + _quantity <= maxSupply,"sold Out");
+    require(walletMint[msg.sender] + _quantity <= maxPerWallet,"exceed max wallet");
+
+    walletMint[msg.sender]= walletMint[msg.sender] + (_quantity);
+
+    for (uint256 i=0;i<_quantity;i++){
+      uint256 newTokenId = totalSupply +1;
+      totalSupply++;
+      _safeMint(msg.sender,newTokenId);
+    }
+
+
+  }
+
+
+  // function mintById(uint256 _tokenId) public payable {
+  //    require(isPublicMintEnabled,"minting not enabled");
+  //    require(msg.value==mintPrice,"wrong mint price");
+  //    require(!_exists(_tokenId),"Token Id exists!");
+  //    require(!tokenId>1000," token Id out of bound" )
+  //    require(walletMint[msg.sender] + 1 <= maxPerWallet,"exceed max wallet");
+  
+  // }
+
+}
---
+++ migrations/2_deploy_dapdogo.js
@@ -0,0 +1,15 @@
+var Dapper_dogo = artifacts.require("./DapDogoNFT.sol");
+
+module.exports = function (deployer) {
+  deployer.deploy(Dapper_dogo).then(async() => {
+    var instance = await Dapper_dogo.deployed();
+    await instance
+      .setBaseTokenUri(
+        ("https://gateway.pinata.cloud/ipfs/QmY63C9Fcx4q8gfmx4CRfFyH5eSCcL8m23qEEboYiLFwqH/")
+      )
+      .then(console.log("base url has been set"));
+    
+    await instance.setPublicMintEnabled(true)
+  }
+  )
+};
